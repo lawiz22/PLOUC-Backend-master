b93fa2fea8d45df9f19c3c58037e59dad4981921
louis martin richard <lawiz2222@gmail.com>
2021-06-01 14:59:33 -0400

    Ok base restframework
    Ok base restframework

--- config/settings/base.py
+++ config/settings/base.py
@@ -69,6 +69,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema', 
     
 }
 
